e9c4351e5881bf1199bb2a82f7ea7cddcf94b184
zelima <ir.mchedlishvili@gmail.com>
2017-03-28 15:25:29 +0400

    [clean up][xl]: removed everything related to ansible and docker - closes #22

--- ansible-playbooks/roles/deploy/files/manager.sh
+++
@@ -1,4 +0,0 @@
-venv=$1
-${venv}/bin/python manager.py dropdb
-${venv}/bin/python manager.py createdb
-${venv}/bin/python manager.py populate
--- dpr-playbook/roles/infra/files/manager.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-venv=$1
-${venv}/exec python manager.py dropdb
-${venv}/exec python manager.py createdb
-${venv}/exec python manager.py populate
