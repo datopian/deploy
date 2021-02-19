34152ce555e1236d34862f5e5589bf58a272f174
zelima <ir.mchedlishvili@gmail.com>
2021-02-19 13:11:26 +0400

    echo variables

--- run_docker_ops.sh
+++ run_docker_ops.sh
@@ -32,15 +32,19 @@ echo "DOCKER_RUN_PARAMS=${DOCKER_RUN_PARAMS}"
 
 [ ! -f "${OPS_SECRET_JSON_FILE}" ] && echo "Missing secret json file ${OPS_SECRET_JSON_FILE}" && exit 1
 
+echo $DATOPIAN_CLOUDSDK_CORE_PROJECT
+echo $CLOUDSDK_CONTAINER_CLUSTER
+echo $CLOUDSDK_COMPUTE_ZONE
+
 ! docker run -i -v "`readlink -f "${OPS_SECRET_JSON_FILE}"`:/k8s-ops/secret.json" \
                  -e "OPS_REPO_SLUG=${OPS_REPO_SLUG}" \
                  -e "OPS_REPO_BRANCH=${OPS_REPO_BRANCH}" \
                  -e "CLOUDSDK_CORE_PROJECT=${DATOPIAN_CLOUDSDK_CORE_PROJECT}" \
-                 -e "CLOUDSDK_CONTAINER_CLUSTER=${DATOPIAN_CLOUDSDK_CORE_PROJECT}" \
-                 -e "CLOUDSDK_COMPUTE_ZONE=${DATOPIAN_CLOUDSDK_CORE_PROJECT}" \
+                 -e "CLOUDSDK_CONTAINER_CLUSTER=${CLOUDSDK_CONTAINER_CLUSTER}" \
+                 -e "CLOUDSDK_COMPUTE_ZONE=${CLOUDSDK_COMPUTE_ZONE}" \
                  $DOCKER_RUN_PARAMS \
                  "${OPS_DOCKER_IMAGE}" \
-                 -c "source ~/.bashrc && source switch_environment.sh ${ENVIRONMENT_NAME}; ${SCRIPT}" \
+                 -c "env | grep CLOUDSDK && source ~/.bashrc && source switch_environment.sh ${ENVIRONMENT_NAME}; ${SCRIPT}" \
     && echo "failed to run SCRIPT" && exit 1
 
 echo "Great Success!"
